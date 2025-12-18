1be8627864a14ee8c4dd3b02e68a4603a9cdd9d1
Olek <alexander.saini10@gmail.com>
2025-12-18 18:34:29 -0500

    Update and rename trend_analysis.m to trend_analysis_lin_reg.m

--- trend_analysis_lin_reg.m
+++ trend_analysis_lin_reg.m
@@ -2,9 +2,10 @@ function [slope, intercept] = trend_analysis(dates, prices)
     % Convert dates to numeric format using `days` from the start date
     numeric_dates = days(dates - dates(1)); % Days from the first date
 
-    % Linear Regression
+    % Linear Regression Formula
     X = [ones(length(numeric_dates), 1), numeric_dates];
     theta = (X' * X) \ (X' * prices); % Normal equation
     intercept = theta(1);
     slope = theta(2);
 end
+
